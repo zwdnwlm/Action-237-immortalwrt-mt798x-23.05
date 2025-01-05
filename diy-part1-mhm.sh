d9d7b1962c23533e78c3d875247545eb9a6b6389
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-01-05 15:13:55 +0800

    Update diy-part1-mhm.sh

--- diy-part1-mhm.sh
+++ diy-part1-mhm.sh
@@ -17,4 +17,4 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
